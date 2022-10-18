07b1b345bd8ffecfa769318cd4f015a6d5b6b51e
JohnnyBakaas <johnny@bakaas-cadconsult.no>
2022-10-18 11:22:53 +0200

    Added collapsing

--- eksemoel 1.js
+++ eksemoel 1.js
@@ -1,5 +1,7 @@
 const card = document.getElementById('card')
 
+const contentOfHTML = []
+
 const noLayout = () => {
     card.classList = '';
 }
@@ -26,4 +28,67 @@ document.getElementById('vertical-layout').addEventListener("click", verticalLay
 document.getElementById('horizontal-layout').addEventListener("click", horizontalLayout);
 
 document.getElementById('grid-layout').addEventListener("click", gridLayout);
+
+const startUp = () => {
+    getHTML()
+    removeHTML()
+    for (var i = 0; i < 5; i++) {
+        console.log(contentOfHTML[i])
+    }
+}
+
+
+
+const getHTML = () => {
+    contentOfHTML.push(document.getElementById('green-card').innerHTML);
+    contentOfHTML.push(document.getElementById('blue-card').innerHTML);
+    contentOfHTML.push(document.getElementById('red-card').innerHTML);
+    contentOfHTML.push(document.getElementById('yellow-card').innerHTML);
+    contentOfHTML.push(document.getElementById('dark-card').innerHTML);
+}
+
+const removeHTML = () => {
+    document.getElementById('green-card').innerHTML = '';
+    document.getElementById('blue-card').innerHTML = '';
+    document.getElementById('red-card').innerHTML = '';
+    document.getElementById('yellow-card').innerHTML = '';
+    document.getElementById('dark-card').innerHTML = '';
+}
+
+document.onload = startUp()
+
+const showGreen = () => {
+    removeHTML()
+    document.getElementById('green-card').innerHTML = contentOfHTML[0];
+}
+
+const showBlue = () => {
+    removeHTML()
+    document.getElementById('blue-card').innerHTML = contentOfHTML[1];
+}
+
+const showRed = () => {
+    removeHTML()
+    document.getElementById('red-card').innerHTML = contentOfHTML[2];
+}
+
+const showYellow = () => {
+    removeHTML()
+    document.getElementById('yellow-card').innerHTML = contentOfHTML[3];
+}
+
+const showDark = () => {
+    removeHTML()
+    document.getElementById('dark-card').innerHTML = contentOfHTML[4];
+}
+
+document.getElementById('green-card-button').addEventListener("click", showGreen)
+
+document.getElementById('blue-card-button').addEventListener("click", showBlue)
+
+document.getElementById('red-card-button').addEventListener("click", showRed)
+
+document.getElementById('yellow-card-button').addEventListener("click", showYellow)
+
+document.getElementById('dark-card-button').addEventListener("click", showDark)
 
