e00512e384ffb62824f37f80636f8d052857eb76
JohnnyBakaas <johnny@bakaas-cadconsult.no>
2022-10-19 10:50:05 +0200

    Ferdig :)

--- eksempel 1.js
+++ eksempel 1.js
@@ -2,6 +2,54 @@ const card = document.getElementById('card')
 
 const contentOfHTML = []
 
+const bodyNr = new Array();
+const headNr = new Array();
+const legsNr = new Array();
+
+var headPosition = 0
+var bodyPosition = 0
+var legPosition = 0
+
+const loadPictures = () => {
+
+    bodyNr[0] = new Image();
+    bodyNr[0].src = 'img/body1.png';
+    
+    bodyNr[1] = new Image();
+    bodyNr[1].src = 'img/body2.png';
+    
+    bodyNr[2] = new Image();
+    bodyNr[2].src = 'img/body3.png';
+    
+    bodyNr[3] = new Image();
+    bodyNr[3].src = 'img/body4.png';
+    
+    headNr[0] = new Image();
+    headNr[0].src = 'img/head1.png';
+    
+    headNr[1] = new Image();
+    headNr[1].src = 'img/head2.png';
+    
+    headNr[2] = new Image();
+    headNr[2].src = 'img/head3.png';
+    
+    headNr[3] = new Image();
+    headNr[3].src = 'img/head4.png';
+    
+    legsNr[0] = new Image();
+    legsNr[0].src = 'img/legs1.png';
+    
+    legsNr[1] = new Image();
+    legsNr[1].src = 'img/legs2.png';
+    
+    legsNr[2] = new Image();
+    legsNr[2].src = 'img/legs3.png';
+    
+    legsNr[3] = new Image();
+    legsNr[3].src = 'img/legs4.png';
+    
+}
+
 const noLayout = () => {
     card.classList = '';
 }
@@ -32,12 +80,8 @@ document.getElementById('grid-layout').addEventListener("click", gridLayout);
 const startUp = () => {
     getHTML()
     removeHTML()
-    for (var i = 0; i < 5; i++) {
-        console.log(contentOfHTML[i])
-    }
+    loadPictures()
 }
-
-
 
 const getHTML = () => {
     contentOfHTML.push(document.getElementById('green-card').innerHTML);
@@ -80,6 +124,15 @@ const showYellow = () => {
 const showDark = () => {
     removeHTML()
     document.getElementById('dark-card').innerHTML = contentOfHTML[4];
+
+    document.getElementById('head-right').addEventListener("click", headRight)
+    document.getElementById('head-left').addEventListener("click", headLeft)
+
+    document.getElementById('body-right').addEventListener("click", bodyRight)
+    document.getElementById('bodt-left').addEventListener("click", bodyLeft)
+
+    document.getElementById('leg-right').addEventListener("click", legRight)
+    document.getElementById('leg-left').addEventListener("click", legLeft)
 }
 
 document.getElementById('green-card-button').addEventListener("click", showGreen)
@@ -92,3 +145,87 @@ document.getElementById('yellow-card-button').addEventListener("click", showYell
 
 document.getElementById('dark-card-button').addEventListener("click", showDark)
 
+// hode kropp og ben starter nedover
+
+const rightArrowImageHead = () => {
+    if(headPosition == 3) {
+        headPosition = 0
+    } else {
+        headPosition++
+    }
+}
+
+const leftArrowImageHead = () => {
+    if(headPosition == 0) {
+        headPosition = 3
+    } else {
+        headPosition -= 1
+    }
+}
+
+const headRight = () => {
+    rightArrowImageHead()
+    document.getElementById('the-head').src = headNr[headPosition].src;
+}
+
+const headLeft = () => {
+    leftArrowImageHead()
+    document.getElementById('the-head').src = headNr[headPosition].src;
+}
+
+
+
+const rightArrowImageBody = () => {
+    if(bodyPosition == 3) {
+        bodyPosition = 0
+    } else {
+        bodyPosition++
+    }
+}
+
+const leftArrowImageBody = () => {
+    if(bodyPosition == 0) {
+        bodyPosition = 3
+    } else {
+        bodyPosition -= 1
+    }
+}
+
+const bodyRight = () => {
+    rightArrowImageBody()
+    document.getElementById('the-body').src = bodyNr[bodyPosition].src;
+}
+
+const bodyLeft = () => {
+    leftArrowImageBody()
+    document.getElementById('the-body').src = bodyNr[bodyPosition].src;
+}
+
+
+
+
+const rightArrowImageLeg = () => {
+    if(legPosition == 3) {
+        legPosition = 0
+    } else {
+        legPosition++
+    }
+}
+
+const leftArrowImageLeg = () => {
+    if(legPosition == 0) {
+        legPosition = 3
+    } else {
+        legPosition -= 1
+    }
+}
+
+const legRight = () => {
+    rightArrowImageLeg()
+    document.getElementById('the-leg').src = legsNr[legPosition].src;
+}
+
+const legLeft = () => {
+    leftArrowImageLeg()
+    document.getElementById('the-leg').src = legsNr[legPosition].src;
+}
